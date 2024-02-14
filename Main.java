76506c53313668d990155d3db81c76be94a19334
Trichards25 <131497949+Trichards25@users.noreply.github.com>
2024-02-14 13:44:20 -0500

    Code

--- Main.java
+++ Main.java
@@ -9,26 +9,63 @@
 *            Reminder: you should never take code from the Internet or another person
 */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Main {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Sphere sun = new Sphere(0.0, 0.0, 0.0, 43.26);
-        Sphere earth = new Sphere(26.0, 42.0, 79.0, 0.395);
-        Sphere planetX = new Sphere(25.3, 43.1, 77.8, 1.4);
+        Sphere[] spheres = new Sphere[3];
+        spheres[0] = new Sphere(0.0, 0.0, 0.0, 43.26);
+        spheres[1] = new Sphere(26.0, 42.0, 79.0, 0.395);
+        spheres[2] = new Sphere(25.3, 43.1, 77.8, 1.4);
 
-        System.out.println("Sun volume: " + sun.volume());
-        System.out.println("Earth volume: " + earth.volume());
-        System.out.println("PlanetX volume: " + planetX.volume());
+        System.out.println("Sun volume: " + spheres[0].volume());
+        System.out.println("Earth volume: " + spheres[1].volume());
+        System.out.println("PlanetX volume: " + spheres[2].volume());
 
-        if (sun.collide(earth)){
+        if (spheres[0].collide(spheres[1])){
             System.out.println("The Sun and Earth collide");
         }
-        if (sun.collide(planetX)){
+        if (spheres[0].collide(spheres[2])){
             System.out.println("The Sun and PlanetX collide");
         }
-        if (earth.collide(planetX)){
+        if (spheres[0].collide(spheres[2])){
             System.out.println("The Earth and PlanetX collide");
         }
+        System.out.println("Enter a location in space");
+        double x = doubleInput("Enter x: ");
+        double y = doubleInput("Enter y: ");
+        double z = doubleInput("Enter z: ");
+
+        double smallestDist = 0.0;
+
+        for(Sphere sphere : spheres){
+            double temp = sphere.Distance(x, y, z);
+            if (temp < smallestDist){
+                smallestDist = temp;
+            }
+        }
+    }
+    public static double doubleInput(String prompt) {
+        //initialize the variables
+        double num = 0;
+        boolean valid = false;
+        //loops until the input is valid
+        while (!valid) {
+            //outputs the prompt
+            System.out.println(prompt);
+            //tries to get the input and set as an double
+            try {
+                num = input.nextDouble();
+                valid = true;
+                //if the program gets an error output invalid input and trys again
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input ");
+                input.next();
+            }
+        }
+        //returns a valid num
+        return num;
     }
 }
--- Sphere.java
+++ Sphere.java
@@ -29,11 +29,11 @@ class Sphere {
     public double volume(){
         return 4.0/3.0 * Math.PI * Math.pow(radius, 3);
     }
-    public double Distance(Sphere sphere2){
-        return Math.sqrt(Math.pow(x - sphere2.x, 2) + Math.pow(y - sphere2.y, 2) + Math.pow(z - sphere2.z , 2));
+    public double Distance(double x, double y, double z){
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2) + Math.pow(this.z - z , 2));
     }
     public boolean collide(Sphere sphere2){
-        double distance = Distance(sphere2);
+        double distance = Distance(sphere2.x, sphere2.y, sphere2.z);
         double radiusSum = radius + sphere2.radius;
         if (distance < radiusSum){
             return true;
