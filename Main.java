d32683d37d38df49e25ce810fa91f0f852835c46
Trichards25 <131497949+Trichards25@users.noreply.github.com>
2024-02-14 13:30:48 -0500

    Code

--- Main.java
+++ Main.java
@@ -13,8 +13,22 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-       System.out.println("Music of the Spheres");
+        Sphere sun = new Sphere(0.0, 0.0, 0.0, 43.26);
+        Sphere earth = new Sphere(26.0, 42.0, 79.0, 0.395);
+        Sphere planetX = new Sphere(25.3, 43.1, 77.8, 1.4);
 
+        System.out.println("Sun volume: " + sun.volume());
+        System.out.println("Earth volume: " + earth.volume());
+        System.out.println("PlanetX volume: " + planetX.volume());
 
+        if (sun.collide(earth)){
+            System.out.println("The Sun and Earth collide");
+        }
+        if (sun.collide(planetX)){
+            System.out.println("The Sun and PlanetX collide");
+        }
+        if (earth.collide(planetX)){
+            System.out.println("The Earth and PlanetX collide");
+        }
     }
 }
--- Sphere.java
+++ Sphere.java
@@ -12,7 +12,32 @@
 
 
 class Sphere {
-    public Sphere(){
+    double x;
+    double y;
+    double z;
+    double radius;
+    public Sphere(double x, double y, double z, double radius){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.radius = radius;
 
+    }
+    public double Area(){
+        return 4.0 * Math.PI * Math.pow(radius, 2);
+    }
+    public double volume(){
+        return 4.0/3.0 * Math.PI * Math.pow(radius, 3);
+    }
+    public double Distance(Sphere sphere2){
+        return Math.sqrt(Math.pow(x - sphere2.x, 2) + Math.pow(y - sphere2.y, 2) + Math.pow(z - sphere2.z , 2));
+    }
+    public boolean collide(Sphere sphere2){
+        double distance = Distance(sphere2);
+        double radiusSum = radius + sphere2.radius;
+        if (distance < radiusSum){
+            return true;
+        }
+        return false;
     }
 }
